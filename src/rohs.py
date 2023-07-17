0dbb4b48cdb5cc1838160b0cb87357bce1b35096
Kartik Chundru <kc18@sanger.ac.uk>
2023-07-17 15:50:08 +0100

    Update rohs.py
    comment function

--- src/rohs.py
+++ src/rohs.py
@@ -3,6 +3,19 @@ import numpy as np
 import pyranges as pr
 
 def calc_roh_overlap(genes, genes_pos, rohs_dir, populations, N_probands, probands_populations, qual_filter=20):
+    """
+    This function calculates the proportion of probands with a ROH overlapping each gene
+    input:
+    genes - list of genes
+    genes_pos - dictionary of genes and gene positions 
+    rohs_dir - directory with ROH files
+    populations - dictionary of populations with samples in those populations
+    N_probands - dictionary with number of number of probands per population
+    probands_populations - dataframe of probands and their population assignment
+    qual_filter - quality filter to use, default 20
+    output:
+    a - dictionary of genes with the proportion of probands with ROH overlapping per population
+    """
     lds=[0.2, 0.4, 0.6, 0.8]
     
     a_tmp={gene:{pop:{ld:0 for ld in lds} for pop in populations} for gene in genes}
