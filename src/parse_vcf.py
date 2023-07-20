ef5663cc76ea20b302e69b6e309ac997fb4e7553
Kartik Chundru <kc18@sanger.ac.uk>
2023-07-20 09:47:34 +0100

    Update parse_vcf.py
    remove idmap option

--- src/parse_vcf.py
+++ src/parse_vcf.py
@@ -11,6 +11,7 @@ import pandas as pd
 import numpy as np
 from scipy import stats
 import csv
+import os
 
 def variant_parser(args):
     """
@@ -21,11 +22,8 @@ def variant_parser(args):
         vcf =   The input vcf file. Either vcf.gz or bcf; index is required
         g2p =   DDG2P genes file downloaded from DECIPHER (https://www.ebi.ac.uk/gene2phenotype/downloads/DDG2P.csv.gz)
         pop =   Populations assignment file. Columns: "ID1 ID2 Pop" - For DDD ID1=stable ID, ID2=VCF ID. For GeneDx put same ID in both
-        unrelpar    =   unrelated parents list. Columns "ID1 ID2" (ID1 and ID2 same as above) 
-        unaffectedpar   =   unaffected parents list. Columns "ID1 ID2" (ID1 and ID2 same as above) 
+        unrelpar    =   unrelated unaffected parents list. Columns "ID1 ID2" (ID1 and ID2 same as above) 
         gen =   gene position file downloaded from biomart; required columns: 'HGNC symbol', 'Chromosome/scaffold name', 'Gene start (bp)', 'Gene end (bp)'
-        gdxblacklist    =   BED file (UCSC) of blacklisted genes from GeneDx
-        idmap   =   sample ID map file for DDD; required columns: 'person_stable_id', 'vcf_id'
         ped =   family PED file; required columns: Family ID, Individual ID, Dad ID, Mum ID, Sex, Affected status
         cadd    =   SNV CADD file (https://cadd.gs.washington.edu/download)
         cadd-indels =   Indels CADD file (https://cadd.gs.washington.edu/download)
@@ -38,6 +36,8 @@ def variant_parser(args):
         mcg =   gene missense constraint file (not sure where this is from, I got it from Joanna Kaplanis)
         mcr =   missesne constraint regions file (https://storage.googleapis.com/gcp-public-data--gnomad/legacy/exac_browser/regional_missense_constraint.tsv)
         pli =   pLI scores (https://storage.googleapis.com/gcp-public-data--gnomad/legacy/exac_browser/forweb_cleaned_exac_r03_march16_z_data_pLI_CNV-final.txt.gz)
+        snv_qc = QC thresholds for SNVs format= GQ,DP,P(AB),VQSLOD,FPASS
+        indel_qc = QC thresholds for indels format= GQ,DP,AB,VQSLOD,FPASS
         chr =   chromosome
         g1  =   start gene index
         g2  =   end gene index
@@ -56,10 +56,9 @@ def variant_parser(args):
     
     samples=x.samples
     genes, gene_symbols = get_gene_pos(args.gen, chrom, g1, g2)
-    ped, parents, unaffected, samples_pop, samples_pop_inds, unrelated_parents_pop_index, populations = process_ped(args.pop, args.unaffectedpar, args.unrelpar, args.ped, samples)
+    ped, parents, unaffected, samples_pop, samples_pop_inds, unrelated_parents_pop_index, populations = process_ped(args.pop, args.unrelpar, args.ped, samples)
     csq = extract_csq(x)
-    
-    id_map=pd.read_csv(args.idmap, sep=r'\s+', low_memory=False)
+
     compliments = {"A":"T","C":"G","G":"C","T":"A"}
 
     gene_csqpos = csq.index('Gene')
@@ -69,15 +68,20 @@ def variant_parser(args):
     # Fomart = [ GQ, DP, P(AB), VQSLOD, FPASS ]
     # SNV QC - DDD GQ>20, DP>7, P(AB)>1e-3, VQSLOD>-2, FPASS>0.5
     # SNV QC - GeneDx GQ>25, DP>10, P(AB)>1e-3, VQSLOD>-2.5, FPASS>0.7 
-    combination_snv = [20,7,1e-3,-2,0.5]
+    # combination_snv = [20,7,1e-3,-2,0.5]
+    combination_snv = args.snv_qc.split(',')
     # INDEL QC - DDD GQ>20, DP>7, AB>0.2, No VQSLOD, FPASS>0.5
     # INDEL QC - GeneDx GQ>30, DP>7, AB>0.3, No VQSLOD, FPASS>0.7
-    combination_indel = [20,7,0.2,-1000,0.5]
+    # combination_indel = [20,7,0.2,-1000,0.5]
+    combination_indel = args.indel_qc.split(',')
 
     csv_header=['stable_id','genotype','is_proband','child_id','child_genotype','dad_id','dad_genotype','mum_id','mum_genotype','child_inheritance','parent_inheritance','n_unaffected_homalt','variant_class',
             'variant_id','worst_vep_annotation_category','worst_vep_annotation','canonical_vep_annotation_category','canonical_vep_annotation','gene.stable.id', 'gene.name', 'HGNC.symbol','chrom','position','ref',
             'alt']+[pop+'_unrelated_parents_popents_af' for pop in populations]+['gnomAD2.1_AFR','gnomAD2.1_AMR','gnomAD2.1_EAS','gnomAD2.1_FIN','gnomAD2.1_NFE','gnomAD2.1_SAS','gnomAD2.1_ASJ',
             'gnomad2.1_nhomalt','population']+['n_unrelated_parents_'+pop for pop in populations]+['CADD_phred','LOFTEE','MPC','REVEL','PrimateAI','VARITYR_LOO','VARITYER_LOO','PolyPhen', 'spliceai']
+
+    if os.path.exists(args.outdir)==False:
+	    os.makedirs(args.outdir)
     
     with open(args.outdir+'/chr'+chrom+'_'+g1+'_'+g2+'_recessive_candidate.txt', 'w') as write_file:
         csvwrite=csv.writer(write_file, delimiter='\t')
@@ -142,11 +146,11 @@ def variant_parser(args):
                                 pop, child_inh, parent_inh = get_inh(samples_pop_inds, parents, ped, samples, gt_types, variant, i)
 
                                 if pop!='':            
-                                    csvwrite.writerow(['NA' if x=='' else x for x in [get_stable_ids(id_map,i), [var if var!=3 else 'NA' for var in gt_types[np.where(np.isin(samples,i))]][0], i in ped,
-                                        get_stable_ids(id_map,parents[i]) if i in parents else 'NA',
+                                    csvwrite.writerow(['NA' if x=='' else x for x in [i, [var if var!=3 else 'NA' for var in gt_types[np.where(np.isin(samples,i))]][0], i in ped,
+                                        i if i in parents else 'NA',
                                         [var if var!=3 else 'NA' for var in gt_types[np.where(np.isin(samples,parents[i]))]][0] if i in parents else 'NA',
-                                        get_stable_ids(id_map,ped[i][0]) if i in ped else 'NA',[var if var!=3 else 'NA' for var in gt_types[np.where(np.isin(samples,ped[i][0]))]][0] if i in ped else 'NA',
-                                        get_stable_ids(id_map,ped[i][1]) if i in ped else 'NA',[var if var!=3 else 'NA' for var in gt_types[np.where(np.isin(samples,ped[i][1]))]][0] if i in ped else 'NA',
+                                        i if i in ped else 'NA',[var if var!=3 else 'NA' for var in gt_types[np.where(np.isin(samples,ped[i][0]))]][0] if i in ped else 'NA',
+                                        i if i in ped else 'NA',[var if var!=3 else 'NA' for var in gt_types[np.where(np.isin(samples,ped[i][1]))]][0] if i in ped else 'NA',
                                         child_inh, parent_inh, sum(gt_types[np.where(np.isin(samples,unaffected))[0]]==2),worst_vep_csq['VARIANT_CLASS'],
                                         str(variant.CHROM)+':'+str(variant.POS)+'_'+variant.REF+'_'+alt,worst_csq_cat, worst_vep_csq['Consequence'] ,vep_csq_cat,vep_csq['Consequence'], gene, gene_symbols[gene][0], gene_symbols[gene][1],variant.CHROM, 
                                         variant.POS,variant.REF, alt] + list(af_pop.values()) + gnomad_af +[gnomad_nhomalt, pop]+[sum(gt_types[unrelated_parents_pop_index[pop]]!=3) for pop in populations]+ 
