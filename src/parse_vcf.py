90b889a1360299be1382de1f2cc19a0e87238a13
Kartik Chundru <kc18@sanger.ac.uk>
2023-07-20 14:48:14 +0100

    Update parse_vcf.py
    Add clinpred and moipred

--- src/parse_vcf.py
+++ src/parse_vcf.py
@@ -56,7 +56,7 @@ def variant_parser(args):
 
     gene_csqpos = csq.index('Gene')
 
-    cadd_tabix, cadd_indels_tabix, revel_tabix, varity_tabix, spliceai_tabix = init_tabix(args)
+    cadd_tabix, cadd_indels_tabix, revel_tabix, varity_tabix, spliceai_tabix, clinpred_tabix, moipred_tabix = init_tabix(args)
 
     # Fomart = [ GQ, DP, P(AB), VQSLOD, FPASS ]
     # SNV QC - DDD GQ>20, DP>7, P(AB)>1e-3, VQSLOD>-2, FPASS>0.5
@@ -71,7 +71,7 @@ def variant_parser(args):
     csv_header=['stable_id','genotype','is_proband','child_id','child_genotype','dad_id','dad_genotype','mum_id','mum_genotype','child_inheritance','parent_inheritance','n_unaffected_homalt','variant_class',
             'variant_id','worst_vep_annotation_category','worst_vep_annotation','canonical_vep_annotation_category','canonical_vep_annotation','gene.stable.id', 'gene.name', 'HGNC.symbol','chrom','position','ref',
             'alt']+[pop+'_unrelated_parents_popents_af' for pop in populations]+['gnomAD2.1_AFR','gnomAD2.1_AMR','gnomAD2.1_EAS','gnomAD2.1_FIN','gnomAD2.1_NFE','gnomAD2.1_SAS','gnomAD2.1_ASJ',
-            'population']+['n_unrelated_parents_'+pop for pop in populations]+['CADD_phred','LOFTEE','REVEL','VARITYR_LOO','VARITYER_LOO','PolyPhen', 'spliceai']
+            'population']+['n_unrelated_parents_'+pop for pop in populations]+['CADD_phred','LOFTEE','REVEL','VARITYR_LOO','VARITYER_LOO','PolyPhen', 'spliceai', 'clinpred', 'moipred_rp']
 
     if os.path.exists(args.outdir)==False:
 	    os.makedirs(args.outdir)
@@ -131,6 +131,8 @@ def variant_parser(args):
                             spliceai = get_spliceai(chrom, variant.POS, variant.REF, alt, compliments, spliceai_tabix)
                             revel = get_REVEL(chrom, variant.POS, variant.REF, alt, compliments, revel_tabix)
                             varity = get_VARITY(chrom, variant.POS, variant.REF, alt, compliments, varity_tabix)
+			    clinpred = get_clinpred(chrom, variant.POS, variant.REF, alt, compliments, clinpred_tabix)
+			    moipred = get_moipred(chrom, variant.POS, variant.REF, alt, compliments, moipred_tabix)
 
                             for i in hethoms:
                                 pop, child_inh, parent_inh = get_inh(samples_pop_inds, parents, ped, samples, gt_types, variant, i)
@@ -144,4 +146,4 @@ def variant_parser(args):
                                         child_inh, parent_inh, sum(gt_types[np.where(np.isin(samples,parents))[0]]==2),worst_vep_csq['VARIANT_CLASS'],
                                         str(variant.CHROM)+':'+str(variant.POS)+'_'+variant.REF+'_'+alt,worst_csq_cat, worst_vep_csq['Consequence'] ,vep_csq_cat,vep_csq['Consequence'], gene, gene_symbols[gene][0], gene_symbols[gene][1],variant.CHROM, 
                                         variant.POS,variant.REF, alt] + list(af_pop.values()) + gnomad_af +[pop]+[sum(gt_types[unrelated_parents_pop_index[pop]]!=3) for pop in populations]+ 
-                                        [cadd_phred, vep_csq['LoF'], revel, varity[0],varity[1],vep_csq['PolyPhen'], spliceai]])
+                                        [cadd_phred, vep_csq['LoF'], revel, varity[0],varity[1],vep_csq['PolyPhen'], spliceai, clinpred, moipred_rp]])
