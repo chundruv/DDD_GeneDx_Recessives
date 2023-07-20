efbce47c085700a3aa361e1ff2d51514f11e56af
chundruv <kc18@sanger.ac.uk>
2023-07-20 13:59:27 +0100

    test

--- parse.sh
+++ parse.sh
@@ -1,14 +1,15 @@
-python parse.py --vcf 1.bcf 
---pop pops.txt 
---gen gene_pos_hg19_protein_coding_only_unique_EnsemblIDs_intersect_baits.txt 
---ped sanger_and_ega_ids.ped 
---cadd whole_genome_SNVs.tsv.gz 
---gmd gnomad.exomes.r2.1.1.sites.${chrom}.vcf.bgz 
---revel revel_scores.txt.gz 
---chr 1 --g1 1 --g2 10 --gmdversion gnomAD2.1 
---unrelpar unrelated_unaffected_parents_KINGgt0.04419417.txt 
---cadd-indels cadd_scores_hg19_v1.6/InDels.tsv.gz 
---varity varity_all_predictions.txt.gz 
---spliceai spliceai_scores.masked.snv.hg19.vcf.gz 
---spliceai-indels spliceai_scores.masked.indel.hg19.vcf.gz 
---outdir outdir
+#!/bin/bash
+
+python parse.py --vcf test/test1.vcf.gz \
+--pop test/population_table.txt \
+--gen data/gene_pos.txt.gz \
+--ped test/pedigree.ped \
+--cadd /nfs/users/nfs_k/kc18/resources/cadd_scores/cadd_scores_hg19_v1.6/whole_genome_SNVs.tsv.gz \
+--revel /nfs/users/nfs_k/kc18/ddd/recwest/data/missense_pathogenicity_scores/revel_scores.txt.gz \
+--chr 1 --g1 1 --g2 10 --gmdversion gnomAD2.1 \
+--unrelpar unrel_par.txt \
+--cadd-indels /nfs/users/nfs_k/kc18/resources/cadd_scores/cadd_scores_hg19_v1.6/InDels.tsv.gz \
+--varity /nfs/users/nfs_k/kc18/ddd/recwest/data/missense_pathogenicity_scores/varity/varity_all_predictions.txt.gz \
+--spliceai /lustre/scratch125/humgen/resources/SpliceAI_data_files/spliceai_scores.masked.snv.hg19.vcf.gz \
+--outdir test/output
+
