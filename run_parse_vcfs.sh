fbc0ecbf908a214f725dc5db5722acc3817c6df1
Kartik Chundru <kc18@sanger.ac.uk>
2023-10-05 18:37:17 +0100

    Update and rename parse.sh to run_parse_vcfs.sh
    update names

--- run_parse_vcfs.sh
+++ run_parse_vcfs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python parse.py --vcf ~/vcfs_ddd/1.bcf \
+python parse_vcfs.py --vcf ~/vcfs_ddd/1.bcf \
 --pop test/population_table.txt \
 --gen data/gene_pos.txt.gz \
 --ped test/pedigree.ped \
